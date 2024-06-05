a8fa827b6f7726a6467feafd7f4d90d673ce81b5
subinDeveloper <subisubin322@gmail.com>
2024-06-05 14:10:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center text-green-500 text-5xl">
+    <h1>hello subon</h1> 
     </div>
   );
 }
